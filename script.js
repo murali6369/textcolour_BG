ba22279cb59e8ecd33f77a67e90d9c51893fce03
murali6369 <165635783+murali6369@users.noreply.github.com>
2024-09-15 16:20:38 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,89 @@
+function myred(){
+    document.getElementById("text").style.color="red";
+}
+function mygreen(){
+    document.getElementById("text").style.color="green";
+}
+function myyellow(){
+    document.getElementById("text").style.color="yellow";
+}
+function myblue(){
+    document.getElementById("text").style.color="blue";
+}
+
+function mycyan(){
+    document.getElementById("text").style.color="cyan";
+}
+
+function myorange(){
+    document.getElementById("text").style.color="orange";
+}
+
+function mybrown(){
+    document.getElementById("text").style.color="brown";
+}
+
+function mygray(){
+    document.getElementById("text").style.color="gray";
+}
+
+function mymagenta(){
+    document.getElementById("text").style.color="magenta";
+}
+
+function mymaroon(){
+    document.getElementById("text").style.color="maroon";
+}
+
+function mypink(){
+    document.getElementById("text").style.color="pink";
+}
+function myreset(){
+    document.getElementById("text").style.color="#000";
+}
+
+
+
+function mybred(){
+    document.getElementById("text").style.background="red";
+}
+function mybgreen(){
+    document.getElementById("text").style.background="green";
+}
+function mybyellow(){
+    document.getElementById("text").style.background="yellow";
+}
+function mybblue(){
+    document.getElementById("text").style.background="blue";
+}
+
+function mybcyan(){
+    document.getElementById("text").style.background="cyan";
+}
+
+function myborange(){
+    document.getElementById("text").style.background="orange";
+}
+
+function mybbrown(){
+    document.getElementById("text").style.background="brown";
+}
+
+function mybgray(){
+    document.getElementById("text").style.background="gray";
+}
+
+function mybmagenta(){
+    document.getElementById("text").style.background="magenta";
+}
+
+function mybmaroon(){
+    document.getElementById("text").style.background="maroon";
+}
+
+function mybpink(){
+    document.getElementById("text").style.background="pink";
+}
+function mybreset(){
+    document.getElementById("text").style.background="white";
+}
